a6859cf8872b6333ea1528051474a3dea847061f
Emii-lia <fiaromiangaly@gmail.com>
2026-05-04 17:59:31 +0300

    fix(app): restoring to the working version

--- src/main/java/tn/esprit/com/App.java
+++ src/main/java/tn/esprit/com/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! We're newly modified" )
+        System.out.println( "Hello World! We're newly modified and fixed" );
     }
 }
